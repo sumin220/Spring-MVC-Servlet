ba2e01e258a09d81561641191cb02cd55464cb15
sonsumin <sonsumin00@gmail.com>
2024-08-31 22:53:56 +0900

    Feat: ArrayListV3 main

---
+++ src/main/java/array/MyArrayListV3Main.java
@@ -0,0 +1,28 @@
+package array;
+
+public class MyArrayListV3Main {
+
+    public static void main(String[] args) {
+        MyArrayListV3 list = new MyArrayListV3();
+
+        list.add("a");
+        list.add("b");
+        list.add("c");
+        System.out.println(list);
+
+        //원하는 위치에 추가
+        System.out.println("addLast");
+        list.add(3, "addLast");
+        System.out.println(list);
+
+        System.out.println("addFirst");
+        list.add(0, "addFirst");
+        System.out.println(list);
+
+        //삭제
+        list.remove(4);
+        System.out.println(list);
+
+
+    }
+}
