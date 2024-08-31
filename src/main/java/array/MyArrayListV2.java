be3515e2616fcb13171371972d471995387d2b29
sonsumin <sonsumin00@gmail.com>
2024-08-31 22:01:28 +0900

    Feat: 배열 연습1

---
+++ src/main/java/array/MyArrayListV2.java
@@ -0,0 +1,50 @@
+package array;
+
+import java.util.Arrays;
+
+public class MyArrayListV2 {
+
+    private static final int DEFAULT_CAPACITY = 5;
+
+    private Object[] elementData;
+    private int size = 0;
+
+    public MyArrayListV2() {
+        elementData = new Object[DEFAULT_CAPACITY];
+    }
+
+    public MyArrayListV2(int initialCapacity) {
+        elementData = new Object[initialCapacity];
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void add (Object e){
+        if (size == elementData.length){
+            grow();
+        }
+        elementData[size] = e;
+        size++;
+    }
+
+    public void grow() {
+        int oldCapacity = elementData.length;
+        int newCapacity = oldCapacity * 2;
+        elementData = Arrays.copyOf(elementData, newCapacity);
+    }
+
+    public Object get(int index){
+        return elementData[index];
+    }
+
+    public Object set(int index, Object element) {
+        Object oldValue = get(index);
+        elementData[index] = element;
+        return oldValue;
+    }
+
+
+}
+
